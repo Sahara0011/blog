1cf81a84ffe001aa4a03d824028d5259ad800a1c
unknown <nematjonovzubayr1@gmail.com>
2024-02-26 19:14:24 +0500

    first commit

---
+++ blog/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+
+from blog.forms import BlogForm
+from blog.models import BlogPost, BlogCategory
+
+
+# Create your views here.
+def blog_list(request):
+    bloglar = BlogPost.objects.all()
+    categories = BlogCategory.objects.all()
+    context = {
+        'blogs': bloglar,
+        'categories': categories
+    }
+    # https://www.bootdey.com/snippets/view/Latest-News-section
+    return render(request, 'blog_list.html', context=context)
+
+
+def blog_create(request):
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blog_list')
+
+        else:
+            print('form is not valid', form.errors)
+    else:
+        form = BlogForm()
+    context = {
+        'form': form
+    }
+
+    return render(request, 'blog_create.html', context=context)
+
+
+def blog_detail():
+    pass
+# https://www.bootdey.com/snippets/view/project-details
